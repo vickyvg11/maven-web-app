597d0c89df98211f5bd16fe9d809565cbc784ef9
vickyvg11 <vickyvg11@gmail.com>
2024-07-29 20:36:46 +0530

    New File Created

---
+++ Jenkins_Docker.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any 
+
+    environment {
+        GIT_BRANCH= 'master'
+        GIT_ID= 'ID_2_git'
+        GIT_URL= 'https://github.com/vickyvg11/maven-web-app.git'
+    }
+
+    stages {
+        stage ('CheckOut the code') {
+            steps {
+                git branch:"${GIT_BRANCH}",
+                credentialsId: "${GIT_ID}",
+                url:"${GIT_URL}"
+            }
+        }
+
+      
+    }
+}
