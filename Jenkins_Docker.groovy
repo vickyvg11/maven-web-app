c6b25e07036200a67a14178f1e21c492cb53379c
vickyvg11 <vickyvg11@gmail.com>
2024-07-30 15:59:35 +0530

    Modified file

--- Jenkins_Docker.groovy
+++ Jenkins_Docker.groovy
@@ -7,20 +7,16 @@ pipeline {
         GIT_URL= 'https://github.com/vickyvg11/maven-web-app.git'
     }
 
-    stages {
-        stage ('CheckOut the code') {
-            steps {
+      stage ('Build app') {
+        agent {
+            docker { image 'maven' }
+        }
+        
+          steps {
                 git branch:"${GIT_BRANCH}",
                 credentialsId: "${GIT_ID}",
                 url:"${GIT_URL}"
             }
-        }
-
-      stage ('Build app') {
-        steps {
-            sh 'ls -lrt'
-            sh 'docker build -t devops893/docker_repo .'
-        }
       }  
       
     }
