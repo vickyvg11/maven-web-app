e25160400a7d2e729e8ed50dfb231efcedcf006e
vickyvg11 <vickyvg11@gmail.com>
2024-07-31 16:26:50 +0530

    Modified file

--- Jenkins_Docker.groovy
+++ Jenkins_Docker.groovy
@@ -1,10 +1,13 @@
 pipeline {
-    agent any 
+    agent {
+        label 'Slave1'
+    }
 
     environment {
         GIT_BRANCH = 'master'
         GIT_ID = 'ID_2_git'
         GIT_URL = 'https://github.com/vickyvg11/maven-web-app.git'
+        
     }
 
     options {
@@ -16,20 +19,61 @@ pipeline {
         stage('Git Checkout') {
             steps {
                 git branch: "${GIT_BRANCH}",
-                     credentialsId: "${GIT_ID}",
-                     url: "${GIT_URL}"
+                    credentialsId: "${GIT_ID}",
+                    url: "${GIT_URL}"
             }
         }
-        stage('Build app') {
-            agent {
-                docker {
-                    image 'maven:latest'
-                    args '-v maven_web_app:/app'
-                }
-            }
+        // stage('Build app') {
+        //     agent {
+        //         docker {
+        //             image 'maven:latest'
+        //             args '-v maven_web_app:/app'
+        //         }
+        //     }
+        //     steps {
+        //         sh 'mvn clean package'
+        //     }
+        // }
+
+        stage ('Compiling the Code') {
             steps {
-                sh 'mvn clean package'
+                sh 'mvn compile'
+            }
+        }
+
+         stage ('Unit test Cases') {
+            steps {
+                script {
+                    try {
+                        sh 'mvn test'
+
+        } catch (Exception e) {
+                    echo "Got the Error while Testing: $e"
             }
         }
     }
+
+        stage ('Packaging the code') {
+            steps {
+                sh 'mvn package'
+            }
+        }
+
+          stage('Load Testing') {
+            steps {
+                sh 'mvn clean verify'
+            }
+        }
+
+        stage ('Docker Build') {
+            steps {
+                sh 'docker build -t my_image . '
+            }
+        }
+
+
+    
+    }
+}
+
 }
